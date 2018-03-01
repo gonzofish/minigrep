ad6e6c1c7d40ab50709bc3efdadd00e0a8c89cce
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-01 11:44:10 -0500

    Reading CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // args[0] is the program itself
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Search for: {}", query);
+    println!("In file: {}", filename);
 }
