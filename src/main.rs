48d61ef5b1bc1939f0694269b185bb026bf91c9e
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-02 08:47:41 -0500

    Made parse_config -> Config::new (simple!)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Search for: {}", config.query);
     println!("In file: {}", config.filename);
@@ -24,13 +24,15 @@ struct Config {
     query: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    // args[0] is the program itself
-    let filename = args[2].clone();
-    let query = args[1].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // args[0] is the program itself
+        let filename = args[2].clone();
+        let query = args[1].clone();
 
-    Config {
-        filename,
-        query,
+        Config {
+            filename,
+            query,
+        }
     }
 }
