12164cec24e96d08b2a996d9477c048d91a180a9
Matt Fehskens <matt.fehskens@gmail.com>
2018-03-04 19:56:19 -0500

    Search function working with test

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,34 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+    let query = "duct";
+
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents),
+    );
+  }
+}
